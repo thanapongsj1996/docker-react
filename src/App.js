e24d7529a24271c28d2989710bf6a827b8e21926
Thanapong Somjai <thanapongsj1996@gmail.com>
2020-01-13 01:18:31 +0700

    comment docker-compose add text (boy)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Thanapong Somjai
+          Thanapong Somjai (BOY)
         </p>
         <a
           className="App-link"
