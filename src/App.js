6f072ebbd2582fd0b2047af2e18e4ebe4c4259b6
Thanapong Somjai <thanapongsj1996@gmail.com>
2020-01-12 17:38:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thanapong Somjai
         </p>
         <a
           className="App-link"
