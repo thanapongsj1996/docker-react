c2f1b8871981e74543b595dea763e4bab6925ee2
Thanapong Somjai <thanapongsj1996@gmail.com>
2020-01-14 10:40:51 +0700

    fix App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { BrowserRouter, Route } from 'react-router-dom'
-
-const App = () => {
-
+function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route exact={true} path="/" component={Home} />
-        <Route exact={true} path="/profile" component={Profile} />
-      </div>
-    </BrowserRouter>
-  )
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Thanapong Somjai (BOY)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// import React from 'react'
+// import Home from './pages/Home'
+// import Profile from './pages/Profile'
+
+// import { BrowserRouter, Route } from 'react-router-dom'
+
+// const App = () => {
+
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <Route exact={true} path="/" component={Home} />
+//         <Route exact={true} path="/profile" component={Profile} />
+//       </div>
+//     </BrowserRouter>
+//   )
+
+// }
+
+// export default App
