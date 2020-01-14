e8543c7cfb56d93bfa53d26421aec4a4a0edf2bb
Thanapong Somjai <thanapongsj1996@gmail.com>
2020-01-14 10:25:59 +0700

    add Home page, Profile page

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:alpine as builder
+
 WORKDIR '/app'
+
 COPY package*.json ./
 RUN npm install
 COPY . .
--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
-function App() {
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Thanapong Somjai (BOYZ)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/profile" component={Profile} />
+      </div>
+    </BrowserRouter>
+  )
+
 }
 
-export default App;
+export default App
